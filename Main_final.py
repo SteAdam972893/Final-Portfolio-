43db358d5e7f0db84e446fbb2e0ff79ec943b6d3
SteAdam972893 <115586223+SteAdam972893@users.noreply.github.com>
2022-12-13 15:04:33 -0600

    Create Main_final.py

---
+++ Main_final.py
@@ -0,0 +1,32 @@
+# main_game.py
+# Steve Marin
+# 11/29/2022
+# This file will run my game and its beginning.
+
+# Import the main character and section files here
+import Main_character
+import Section_1
+import Section_2
+import Section_3
+import Section_4
+# introduction text to the user
+
+
+
+
+
+print("The Evil Touch!")
+print("Click enter to begin")
+input()
+
+# Elle is in her bedroom
+name = input ("In a cold fall night around 8:30 pm and elle is in her bedroom and hears a loud sound in her bedroom")
+print("The window breaks she begins to panic as to who is breaking in and sees a large man with a hat and beard getting inside")
+
+# Wizard breaks in elle bedroom through window and speaks to her
+print("The wizard talks to elle as she is terrified what should she do hide or talk to wizard?")
+# Wizard takes elle as her brother steps in her bedroom
+print(" or run away or begin attacking the wizard? Tim her younger brother sees whats going on through the door")
+#section 1 begins
+Section_1.start()
+
